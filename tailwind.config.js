e0c26be6f90dd779dd2235d4f997f0706977f8ee
khair335 <abulkhair6363@gmail.com>
2024-07-11 03:09:59 +0600

    sdsa

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'xxl': '1380px', // Custom breakpoint at 480px
+      },
+    },
   },
   plugins: [],
 }
